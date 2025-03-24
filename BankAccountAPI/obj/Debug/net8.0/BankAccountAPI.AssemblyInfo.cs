22872779e80cb666885fe9c0299c222fc45549a3
backenddev21 <121293315+backenddev21@users.noreply.github.com>
2025-03-24 02:41:18 +0000

    update version

--- BankAccountAPI/obj/Debug/net8.0/BankAccountAPI.AssemblyInfo.cs
+++ BankAccountAPI/obj/Debug/net8.0/BankAccountAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankAccountAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+461e5f2d4876f607f9304b19acf69310ec0668c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ad59690d5e85f9f7d1ef82e78167dfe3676dfcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankAccountAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankAccountAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
